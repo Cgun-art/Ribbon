7f022723f1bc88d03714c4e9127de6f9e28f48e5
Cgun-art | michael :) <migueloproio903@gmail.com>
2026-05-13 14:27:50 -0300

    Create physics.lua

---
+++ lib/physics.lua
@@ -0,0 +1,5 @@
+dimensions = x, y
+
+add Collider.obj = Fill withMesh(coords, facesLimit (4096), coordpixels, addfaces, concludeafterLimitReached)
+add physics.obj = Solid, bounce(r, 0), gravity)
+tangent.normals = collider (Y-)
