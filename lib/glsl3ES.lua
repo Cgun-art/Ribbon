42667f84ae6e52c337b04ab93d52f4049792c811
Cgun-art | michael :) <migueloproio903@gmail.com>
2026-05-13 14:23:22 -0300

    Create lib/glsl3ES.lua

---
+++ lib/glsl3ES.lua
@@ -0,0 +1,30 @@
+// This uses an custom render engine
+
+local function Render = GL ES 3.0
+
+MaxSamples = 16
+Samples = 8
+Settings:
+RenderNorm16 = true
+DepthClamp = false
+Dimensions = x, y
+RayCasting = true
+CompileType = usedevice
+CompileRender = usedevice
+
+if dim X = CreateNewPixel:
+CreateNewPixel( = true
+CreateNewPixel = true
+color.space = rgb
+color.space.id = bt255
+
+if dim Y = CreateNewPixel:
+CreateNewPixel( = true
+CreateNewPixel = true
+color.space = rgb
+color.space.id = bt255
+
+if sample.limit = Reached:
+change.pixel.color = r.255, g.0, b.0
+
+local function ESSL = false
